a97847648af8b9e74bcf14016d3c1f749f02d13a
Simpler1 <kevin.bezold@gmail.com>
2018-05-05 18:13:46 -0400

    step_7 images

--- full_steps/lib/main.dart
+++ full_steps/lib/main.dart
@@ -3,6 +3,8 @@
 // BSD-style license that can be found in the LICENSE file.
 
 import 'dart:async';
+import 'dart:math';
+import 'dart:io';
 import 'package:flutter/material.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/foundation.dart';
@@ -11,6 +13,8 @@ import 'package:firebase_analytics/firebase_analytics.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_database/firebase_database.dart';
 import 'package:firebase_database/ui/firebase_animated_list.dart';
+import 'package:firebase_storage/firebase_storage.dart';
+import 'package:image_picker/image_picker.dart';
 
 final reference = FirebaseDatabase.instance.reference().child('messages');
 final auth = FirebaseAuth.instance;
@@ -75,7 +79,12 @@ class ChatMessage extends StatelessWidget {
                       style: Theme.of(context).textTheme.subhead),
                   new Container(
                     margin: const EdgeInsets.only(top: 5.0),
-                    child: new Text(snapshot.value['text']),
+                    child: snapshot.value['imageUrl'] != null
+                        ? new Image.network(
+                            snapshot.value['imageUrl'],
+                            width: 250.0,
+                          )
+                        : new Text(snapshot.value['text']),
                   ),
                 ],
               ),
@@ -139,6 +148,22 @@ class ChatScreenState extends State<ChatScreen> {
       child: new Container(
           margin: const EdgeInsets.symmetric(horizontal: 8.0),
           child: new Row(children: <Widget>[
+            new Container(
+              margin: new EdgeInsets.symmetric(horizontal: 4.0),
+              child: new IconButton(
+                icon: new Icon(Icons.photo_camera),
+                onPressed: () async {
+                  await _ensureLoggedIn();
+                  File imageFile = await ImagePicker.pickImage();
+                  int random = new Random().nextInt(100000);
+                  StorageReference ref =
+                      FirebaseStorage.instance.ref().child("image_$random.jpg");
+                  StorageUploadTask uploadTask = ref.put(imageFile);
+                  Uri downloadUrl = (await uploadTask.future).downloadUrl;
+                  _sendMessage(imageUrl: downloadUrl.toString());
+                },
+              ),
+            ),
             new Flexible(
               child: new TextField(
                 controller: _textController,
@@ -202,9 +227,10 @@ class ChatScreenState extends State<ChatScreen> {
     }
   }
 
-  void _sendMessage({String text}) {
+  void _sendMessage({String text, String imageUrl}) {
     reference.push().set({
       'text': text,
+      'imageUrl': imageUrl,
       'senderName': googleSignIn.currentUser.displayName,
       'senderPhotoUrl': googleSignIn.currentUser.photoUrl,
     });
